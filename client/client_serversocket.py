81b741d308eaa679cc9204a9647ff82e2baa49a0
JabalnurIT <jabalnur.it@gmail.com>
2023-02-25 22:28:13 +0700

    finished client and server serversocket

--- client/client_serversocket.py
+++ client/client_serversocket.py
@@ -1,17 +1,45 @@
 import socket
 import sys
+import tqdm
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
+input = sys.stdin.readline()
+input = input.split(" ")
+command = input[0]
+filename = input[1]
 
-    # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8")
+if command == "download":
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(filename + "\n", "utf-8"))
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+        # Receive data from the server and shut down
+        header = str(sock.recv(1024), "utf-8")
+        filename = header.split("\n")[0].split(" ")[-1]
+        filesize = header.split("\n")[1].split(" ")[-1]
+
+        file = open("client/files/"+filename, "wb")
+
+        file_bytes = b"ada"
+
+        success = False
+
+        progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(filesize))
+
+        while not success:
+            data = sock.recv(1024)
+            if file_bytes[-5:] == b"<END>":
+                success = True
+            else:
+                file_bytes += data
+            progress.update(1024)
+        
+        file.write(file_bytes)
+
+        file.close
+
+    # print("Sent:     {}".format(data))
+    # print("Received: {}".format(received))
 
--- server/server_serversocket.py
+++ server/server_serversocket.py
@@ -1,3 +1,4 @@
+import os
 import socketserver
 
 # https://docs.python.org/3/library/socketserver.html
@@ -12,11 +13,25 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        self.filename = self.request.recv(1024).strip().decode("utf-8")
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        # print(self.data)
+        path_file = "server/files/" + self.filename
+        file = open(path_file, "rb")
+        file_size = os.path.getsize(path_file)
+        data = file.read()
+        # print(file_size)
+
+        header = "file-name: "+self.filename+"\n"
+        header = header + "file-size: "+str(file_size)+"\n\n\n"
+
+        self.request.send(header.encode())
+
+        self.request.sendall(data)
+
+        self.request.send(b"<END>")
+
+        file.close()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
